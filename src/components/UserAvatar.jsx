5e8f695ba7523547b2e921faed506b6f186e7784
César Gabriel Leañez <93480406+cesarleaz@users.noreply.github.com>
2025-03-16 17:27:14 -0400

    Fix user photo
    Use the github photo from @midudev's profile so there is no problem loading the photo

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -2,7 +2,7 @@ export function UserAvatar() {
   return (
     <img
       alt='Foto de midu'
-      src='https://pbs.twimg.com/profile_images/1613612257015128065/oA0Is67J_400x400.jpg'
+      src='https://avatars.githubusercontent.com/u/1561955?v=4'
     />
   )
 }
